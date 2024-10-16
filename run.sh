d914813644de322c27fd059003b8901e272f463f
DMGDy <dylangarza1909@gmail.com>
2024-10-16 03:54:22 -0400

    [build script] creating a bin directory if for .class

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
 
 PROJECT="CreatePurchase"
-SRC="./src"
-BIN="./bin"
+SRC="src"
+BIN="bin"
+
+if [ ! -d ${BIN} ]; then
+  echo "Creating directory ${BIN} for binary files"
+  mkdir -p ${BIN}
+else
+  :
+fi
 
 echo "Compiling project ${PROJECT}..."
 javac -d ${BIN} ${SRC}/*.java
