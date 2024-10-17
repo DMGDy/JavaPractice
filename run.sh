56a6077b1b7b28bbf53faf0ad2ff95ce4837e756
DMGDy <dylangarza1909@gmail.com>
2024-10-16 20:21:02 -0400

    [build script] removing empty else block

--- run.sh
+++ run.sh
@@ -7,15 +7,12 @@ BIN="bin"
 if [ ! -d ${BIN} ]; then
   echo "Creating directory ${BIN} for binary files"
   mkdir -p ${BIN}
-else
-  :
 fi
 
+# compile project sources
 echo "Compiling project ${PROJECT}..."
 javac -d ${BIN} ${SRC}/*.java
 printf "Compilation successful! now running...\n\n"
 
+# run the build in jvm
 java -cp ${BIN} ${PROJECT}
-
-
-
